5023b97440e1a623e839505d5d7057c88312f0d6
Michael Bonner <michaelwbonner@gmail.com>
2023-06-15 16:57:15 -0600

    example functionality

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="py-4 px-4 lg:px-8 max-w-7xl mx-auto">
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,96 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { format } from 'date-fns';
+
+	let now = new Date();
+
+	let testFormat = 'yyyy-MM-dd HH:mm:ss';
+
+	let testFormatDateString = '';
+
+	setInterval(() => {
+		now = new Date();
+
+		try {
+			testFormatDateString = format(now, testFormat);
+		} catch (error) {
+			testFormatDateString = 'Invalid format';
+		}
+	}, 500);
+
+	const formatStrings = [
+		'Pp',
+		'PPpp',
+		'PP',
+		'PPP',
+		"PP 'at' p",
+		'MM/dd/Y hh:mm a',
+		"iii M/d 'at' p",
+		'yyyy-MM-dd HH:mm:ss',
+		'MM/dd/yyyy',
+		'yyyy-MM-dd',
+		'yyyy/MM/dd',
+		'MMMM yyyy',
+		'MMM d',
+		'MMMM d',
+		'eeee MM/dd/yyyy',
+		'E MMM do',
+		"MMMM d 'at' p",
+		'MMMM d, yyyy',
+		'eee',
+		'eeee',
+		'h:mm a'
+	];
+</script>
+
+<div class="grid gap-12">
+	<h1 class="text-3xl font-bold underline underline-offset-4">
+		<code>date-fns</code> format helper
+	</h1>
+	<div class="text-2xl">
+		<span>Reference Time:</span>
+		<span>
+			{now.toLocaleDateString()}
+			{now.toLocaleTimeString()}
+		</span>
+	</div>
+	<div class="grid gap-4">
+		<h2 class="text-2xl font-bold">Test</h2>
+		<div class="grid gap-1 border py-2 px-4 bg-white">
+			<dt class="text-sm">
+				<label>
+					<input
+						bind:value={testFormat}
+						class="border py-2 px-4 text-sm rounded"
+						placeholder="Test format string"
+						type="text"
+					/>
+				</label>
+			</dt>
+			<dd class="font-bold">{testFormatDateString}</dd>
+		</div>
+	</div>
+	<div class="grid gap-4">
+		<h2 class="text-2xl font-bold">Useful Starters</h2>
+
+		<div class="grid md:grid-cols-2 xl:grid-cols-3 gap-x-4 gap-y-2">
+			{#each formatStrings as formatString}
+				<div class="grid gap-1 border py-2 px-4 bg-white">
+					<dt class="text-sm">
+						<input
+							disabled
+							class="py-1 px-2 italic text-sm border border-dashed border-gray-300 rounded"
+							value={formatString}
+						/>
+					</dt>
+					<dd class="font-bold">{format(now, formatString)}</dd>
+				</div>
+			{/each}
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
